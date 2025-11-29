c5c067d520875af129e58eefd9da7c0e1807a05d
morufuonline <morufuonline@gmail.com>
2025-11-29 21:22:45 +0000

    Update About.js

--- src/webpages/About.js
+++ src/webpages/About.js
@@ -9,7 +9,7 @@ const About = () => {
 
         <p>Àjokéelewu was founded in 1970 by my parents, TItilayo and Ayinla, to educate on the use of both African and contemporary prints as a way of preserving our African culture. When they started this career path, it was called Riyfat Fashion and they managed this business until year 2000. Today, Riyat Fashion has become Àjokéelewu (which means dressmaker in the South Western region of Nigeria).</p>
 
-        <h3>Our Core Value</h3>
+        <h3>Our Core Value</h3> 
 
         <p>We are a fashion brand that use African prints to make high-end fashion and everyday wears for adults and children. Each fabric has a story to tell and our designs are crafted in a way that helps tell these stories. Àjokéelewu is a reflection of who we are as Africans through fashion.</p>
 
